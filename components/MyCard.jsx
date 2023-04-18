a46e6494a18562113c1ea46548147c1790233dd1
ZildjianVito <zildjianvito07@gmail.com>
2023-04-18 16:13:53 +0700

    add card on right column

---
+++ components/MyCard.jsx
@@ -0,0 +1,34 @@
+import { Box, Flex, Text, Icon, Heading } from "@chakra-ui/react";
+
+import { FiCreditCard } from "react-icons/fi";
+
+export default function MyCard({ bgGradient, balance, cardNumber, validThru }) {
+  return (
+    <Box mt="4" p="4" bgGradient={bgGradient} borderRadius="3xl" color="white">
+      <Flex justifyContent="space-between" align="center">
+        <Text color="gray.300">Current Balance</Text>
+        <Flex justify="center" align="center" columnGap="2">
+          <Icon as={FiCreditCard} />
+          <Text>Vito.</Text>
+        </Flex>
+      </Flex>
+      <Heading as="h3" fontSize="xl" mt="1">
+        ${balance}
+      </Heading>
+      <Text mt="1">**** **** **** {cardNumber}</Text>
+      <Flex mt="4" justify="space-between">
+        <Flex columnGap="4">
+          <Flex flexDirection="column">
+            <Text fontSize="xs">VALID THRU</Text>
+            <Text fontSize="lg">{validThru}</Text>
+          </Flex>
+          <Flex flexDirection="column">
+            <Text fontSize="xs">CVV</Text>
+            <Text fontSize="lg">***</Text>
+          </Flex>
+        </Flex>
+        <Icon as={FiCreditCard} alignSelf="flex-end" />
+      </Flex>
+    </Box>
+  );
+}
--- components/RightColumn.jsx
+++ components/RightColumn.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Flex,
   Heading,
@@ -14,16 +15,78 @@ import {
   InputGroup,
   InputLeftElement,
 } from "@chakra-ui/react";
+import { color } from "framer-motion";
+import { FiPlus, FiCreditCard, FiSearch, FiBell } from "react-icons/fi";
+import MyCard from "./MyCard";
 
 export default function RightColumn() {
+  const [display, setDisplay] = useState("hidden");
+  const [value, setValue] = useState(1);
+
   return (
     <Flex
       flexDir="column"
-      w="35%"
+      w="30%"
       h="100vh"
       bgColor="#F5F5F5"
       p="3%"
       overflow="auto"
-    ></Flex>
+    >
+      <Flex alignContent="center">
+        <InputGroup borderRadius="lg">
+          <InputLeftElement
+            pointerEvents="none"
+            children={<FiSearch color="gray" />}
+          />
+          <Input
+            bgColor="white"
+            type="number"
+            placeholder="Search"
+            borderRadius="lg"
+            w="95%"
+            border="none"
+          />
+        </InputGroup>
+        <IconButton
+          mr="4"
+          icon={<FiBell />}
+          bgColor="white"
+          borderRadius="full"
+          fontSize="sm"
+        />
+        <Flex
+          w={25}
+          h={25}
+          p="2"
+          bgColor="pink.300"
+          borderRadius="full"
+          alignItems="center"
+          fontSize="xs"
+          color="white"
+          ml="-7"
+          mt="-2"
+          justify="center"
+          zIndex="4"
+        >
+          2
+        </Flex>
+      </Flex>
+      <Heading mt="4" letterSpacing="tight">
+        My Cards
+      </Heading>
+      {value === 1 && (
+        <MyCard
+          bgGradient={"linear(to-b,pink.500,green.200)"}
+          balance={"2,781.99"}
+          cardNumber={"9183"}
+          validThru={"11/24"}
+        />
+      )}
+      <Flex align="center" justify="center" columnGap="3" mt="2">
+        <Button bgColor="gray.700" borderRadius="md" />
+        <Button bgColor="gray.700" borderRadius="md" />
+        <Button bgColor="gray.700" borderRadius="md" />
+      </Flex>
+    </Flex>
   );
 }
